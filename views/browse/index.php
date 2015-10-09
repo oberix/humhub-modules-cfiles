8ed543176446df92b57056115302eccd207da21d
Marco Pattaro <mpattaro@gmail.com>
2015-10-09 20:15:26 +0200

    add missing parens

--- views/browse/index.php
+++ views/browse/index.php
@@ -31,7 +31,7 @@ $this->registerJsVar('cfilesEditFolderUrl', $contentContainer->createUrl('/cfile
                         </span>
                     </li>
                     <li class="nav-divider"></li>
-                    <li><?php echo Html::a('Add directory', $contentContainer->createUrl('/cfiles/browse/edit-folder', ['fid' => $folderId]), array'data-target' => '#globalModal')); ?></li>
+                    <li><?php echo Html::a('Add directory', $contentContainer->createUrl('/cfiles/browse/edit-folder', ['fid' => $folderId]), array('data-target' => '#globalModal')); ?></li>
                     <?php if ($folderId !== 0) : ?>
                         <li><?php echo Html::a('Edit directory', $contentContainer->createUrl('/cfiles/browse/edit-folder', ['id' => $folderId]), array('data-target' => '#globalModal')); ?></li>
                     <?php endif; ?>
